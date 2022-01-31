e7786c39b9334bdc196c2395dd7d85e0e085e7ed
Shu-Ting Huang <sthuang890702@gmail.com>
2022-01-31 18:30:14 -0500

    Create function next_row_op()

--- __init__.py
+++ __init__.py
@@ -20,3 +20,29 @@ class RowOpSequence:
     def add_step(self, row_op):
         self.matrices.append(simplify(self.matrices[-1].elementary_row_op(**row_op)))
         self.row_ops.append(row_op)
+
+def next_row_op(A):
+    # A is empty matrix
+    if len(A) == 0:
+        return None
+
+    # The first column of A is zero vector
+    elif A[:,0].is_zero_matrix:
+        return next_row_op(A[:,1:])
+
+    # A[0,0] is zero
+    elif A[0,0] == 0:
+        return {"op": "n<->m", "row1": 0, "row2": min(i for i in range(1, A.shape[0]) if A[i,0] != 0)}
+    
+    # A[0,0] is the only non-zero entry in the first column
+    elif A[1:,0].is_zero_matrix:
+        temp_row_op = next_row_op(A[1:,1:])
+        for k in temp_row_op.keys():
+            if k.startswith("row"):
+                temp_row_op[k] += 1
+        return temp_row_op
+    
+    # A[1:,0] has some non-zero entries
+    else:
+        i = min(i for i in range(1, A.shape[0]) if A[i,0] != 0)
+        return {"op": "n->n+km", "k": -A[i,0]/A[0,0], "row": i, "row2": 0}
