98e4b70ad6aa98d6ab5a1dd9ff2dfdf6bdcb2e85
Shu-Ting Huang <sthuang890702@gmail.com>
2022-02-01 02:44:24 -0500

    Test with finite field of prime order

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from __init__ import RowOpSequence, next_row_op_Gauss_Jordan
 
-from sympy import Matrix, Rational, Symbol, eye
+from sympy import Matrix, Rational, Symbol, eye, GF
 from random import choice
 
 for _ in range(5):
@@ -20,6 +20,16 @@ assert row_op_seq.matrices[-1][:,0:2] == eye(2)
 assert row_op_seq.matrices[-1][:,2:4]*(Symbol('a')*Symbol('d')-Symbol('b')*Symbol('c')) == Matrix([['d', '-b'], ['-c', 'a']])
 assert len(row_op_seq.row_ops) == 4
 assert len(row_op_seq.matrices) == 5
+
+K = GF(3571) # 3571 is the 500th prime
+for _ in range(5):
+    m = choice(range(1,15))
+    n = choice(range(1,15))
+    A = Matrix(m, n, lambda i,j: K(choice(range(3571))))
+    row_op_seq = RowOpSequence(A)
+    while next_row_op_Gauss_Jordan(row_op_seq.matrices[-1]) != None:
+        row_op_seq.add_step(next_row_op_Gauss_Jordan(row_op_seq.matrices[-1]))
+    assert row_op_seq.matrices[-1] == A.rref()[0]
 
 
 print("\nAll tests are passed")
