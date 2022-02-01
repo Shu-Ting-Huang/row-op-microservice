760ee2c658248ba1dac203e6f4c7ec674215346b
Shu-Ting Huang <sthuang890702@gmail.com>
2022-02-01 01:26:53 -0500

    Rename a variable

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ for _ in range(5):
     m = choice(range(1,15))
     n = choice(range(1,15))
     A = Matrix(m, n, lambda i,j: Rational(choice(range(-100,100)),choice(range(1,10))))
-    seq = RowOpSequence(A)
-    while next_row_op_Gauss_Jordan(seq.matrices[-1]) != None:
-        seq.add_step(next_row_op_Gauss_Jordan(seq.matrices[-1]))
-    assert seq.matrices[-1] == A.rref()[0]
+    row_op_seq = RowOpSequence(A)
+    while next_row_op_Gauss_Jordan(row_op_seq.matrices[-1]) != None:
+        row_op_seq.add_step(next_row_op_Gauss_Jordan(row_op_seq.matrices[-1]))
+    assert row_op_seq.matrices[-1] == A.rref()[0]
 
 print("\nAll tests are passed")
