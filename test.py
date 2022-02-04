b4e580bb3578cc15e857400187319d6d7e65729d
Shu-Ting Huang <sthuang890702@gmail.com>
2022-02-04 02:56:27 -0500

    Create function random_rational() in test.py

--- test.py
+++ test.py
@@ -3,10 +3,13 @@ from __init__ import RowOpSequence, next_row_op_Gauss_Jordan
 from sympy import Matrix, Rational, Symbol, eye, GF, prime
 from random import choice
 
+def random_rational():
+    return Rational(choice(range(-100,100)), choice(range(1,50)))
+
 for _ in range(5):
     m = choice(range(1,15))
     n = choice(range(1,15))
-    A = Matrix(m, n, lambda i,j: Rational(choice(range(-100,100)),choice(range(1,10))))
+    A = Matrix(m, n, lambda i,j: random_rational())
     row_op_seq = RowOpSequence(A)
     while next_row_op_Gauss_Jordan(row_op_seq.matrices[-1]) != None:
         row_op_seq.add_step(next_row_op_Gauss_Jordan(row_op_seq.matrices[-1]))
